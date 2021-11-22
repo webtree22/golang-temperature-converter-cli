e5ecc2f705ac6842bc13ced29a7ba8bb6004a1f0
Sudeep Gurung <webtree22@gmail.com>
2021-11-22 19:01:56 +0545

    wip

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ var shouldConvertAgain string
 
 var err error
 
-var errInvalidArguments = errors.New("Invalid arguments")
-var errReadingInput = errors.New("Error reading input")
+var errInvalidArguments = errors.New("invalid arguments")
+var errReadingInput = errors.New("error reading input")
 
 func main() {
 	if len(os.Args) != 2 {
@@ -23,12 +23,23 @@ func main() {
 	}
 	originUnit = strings.ToUpper(os.Args[1])
 
+	// Inside the for loop, below the first fmt.Print() statement which prints "What is the current temperature in...", invoke the fmt.Scanln() function, passing &originValue as the argument. Assign the two return values to the variables _ and err respectively. On the following line, create an if statement checking if err != nil, and if that's true, invoke the printError() function, passing errReadingInput as its argument.
+
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-
+		_, err = fmt.Scanln(&originUnit)
+		if err != nil {
+			printError(errReadingInput)
+		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&originValue)
+		if err != nil {
+			printError((errReadingInput))
+		}
 
-		if shouldConvertAgain != "Y" {
+		// Currently, we can't always be sure that the value assigned to shouldConvertAgain is going to be in the casing we expect. Let's fix this by making a small change to the condition on the last if statement inside the for loop. The condition should be a combination of calling the strings.ToUpper() function, passing it the result of calling the strings.TrimSpace() function with shouldConvertAgain as its argument. If the result of all of that is NOT equal to "Y", then the existing if block containing fmt.Println("Good bye!") followed by break should be run.
+
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
